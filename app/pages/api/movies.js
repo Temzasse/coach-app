df7a2dbfe08a0447e41c164bfbda5ef2bc1a1ce0
Teemu Taskula <themetor@gmail.com>
2020-08-22 23:17:20 +0300

    Fix movie example

--- app/pages/api/movies.js
+++ app/pages/api/movies.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 export default async function (req, res) {
   if (req.method === "POST") {
     const { body } = req;
-    const movie = await prisma.movie.create({ data: JSON.parse(body) });
+    const movie = await prisma.movie.create({ data: body });
     res.json(movie);
   }
 }
